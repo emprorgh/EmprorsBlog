0e32c70a207be842f51c8ba938c3dc4712213438
Sujan Ghimire <sujangh10@gmail.com>
2020-05-21 09:10:24 -0400

    Changes-5-21-Added bootstrap navigation and angular grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RTAutomationUI';
+ 
+
+  columnDefs = [
+    {headerName : 'Product ID', field: 'Id', filter: true},
+    {headerName : 'Application Name', field: 'Product1'},
+    {headerName : 'Web address', field: 'Url'}
+  ]
+
+  rowData =[
+        { Id: '11', Product1: 'SSMD', Url: 'www.singlesourceqat.com'},
+        { Id: '20', Product1: 'SSDP', Url: 'www.google.com' },
+        { Id: '23', Product1: 'Patents', Url: '##' }
+
+  ];
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AgGridModule} from 'ag-grid-angular';
+import { MainNavbarComponent } from './main-navbar/main-navbar.component';
+import {NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './products/product/product.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavbarComponent,
+    ProductsComponent,
+    ProductComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule.withComponents([]),
+    NgbModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
